1e8dc1651a7962b7fcaf495b8cef2afaeb21106d
Mark <Mark@Juhoonui-MacBook-Pro.local>
2014-09-20 10:34:04 +0900

    opinion v1

--- py/rep_opinion.py
+++ py/rep_opinion.py
@@ -1,22 +1,30 @@
 import psycopg2
 
+# connect to db
 try:
-    conn = psycopg2.connect("dbname='uzeni' user='postgres' host='192.168.50.188' password='dbwpsdkdl'")
+    #conn = psycopg2.connect("dbname='uzeni' user='postgres' host='192.168.50.188' password='dbwpsdkdl'")
+    conn = psycopg2.connect("dbname='Mark' user='Mark' host='localhost' password=''")
 except:
     print "I am unable to connect to the database"
 
 stack = {}
 
+# open cursor
 cur = conn.cursor()
-cur.execute("select * from water_korea_dump;")
-for rec in cur:
-	seq, target, num, link, value, yn = rec
-	print (value)
-	var = raw_input("Please your decision (1:good or 2:bad) : ")
-	stack[seq]=yn
+cur.execute("select * from water_korea_dump where rep is null") 
 
-for str in stack:
-	print (str.value)
+# input your opinion to docuemnts. 
+for rec in cur:
+	seq, target, num, link, body, rep = rec
+	print (seq,target,body)
+	var = raw_input("Please your decision (1:good or 2:bad) : ")
+	stack[str(seq)] = 'Good' if int(var) == 1 else 'Bad'
+
+# update reputation field.
+for seq in stack.keys():
+	stmt = "update water_korea_dump set rep = '" + stack[seq] + "' where seq = " + seq
+	print stmt
+	cur.execute(stmt)
 
 conn.commit()
 conn.close()
