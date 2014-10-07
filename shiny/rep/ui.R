be7583ef822d517bee47692d8b14901d0384816c
Mark <juhoon.kim@me.com>
2014-10-07 18:40:02 +0900

    Update ui.R

--- shiny/rep/ui.R
+++ shiny/rep/ui.R
@@ -1,33 +1,67 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for dataset viewer application
 shinyUI(fluidPage(
 
-  # Application title
-  titlePanel("Smile with Water Korea"),
+  titlePanel("UZENi Reputation Monitor"),
+
+  fluidRow(
+  column(12, h5("인터넷상에서 키워드 및 연관어 검색을 통하여 평판을 수집하여, 긍.부정 현황과 트랜드 정보를 제공 합니다!"))
+  #,
+  #column(4, "")
+  ),
+  
+  hr(),
+  uiOutput("code") 
+  ,
+
+  fluidRow(
+    p("* 글자색 : ",
+      span("빨간색", style = "color:red"),
+      " - 평판이 평균 이하, ",
+      span("파란색", style = "color:blue"),
+      " - 평판이 평균에 근접, ",
+      span("초록색", style = "color:green"),
+      " - 평판이 평균 이상"
+    ),
+    p("* 글자내용 : ",
+      strong("상승"),
+      " - 평판이 좋아지고 있음, ",
+      strong("유지"), 
+      " - 평판이 현상태를 유지함, ", 
+      strong("하락"), 
+      " - 평판이 나빠지고 있음")
+  ),
+  hr(),
+  
+  fluidRow(
+    h4("기간선택:"),
+    sliderInput("bins",
+                "출력일수(day): ",
+                min = 1,
+                max = 30,
+                value = 7)
+  ),
+  fluidRow(
+    column(7,
+           plotOutput("PlotB")
+    ),
+    column(5,
+           plotOutput("PlotC")
+    )
+  ),
+  fluidRow(
+    column(12,
+           plotOutput("PlotA")
+    )
+  ),
+  
+  
+  hr(),
+  
+  fluidRow(
+    dataTableOutput(outputId="table")
+  )    
 
 
-fluidRow(
-	h4("input area"),
-        sliderInput("bins",
-                    "Start ago (day): ",
-                    min = 1,
-                    max = 30,
-                    value = 7)
-)
-,
-fluidRow(
-	column(7,
-		plotOutput("PlotB")
-	),
-	column(5,
-		plotOutput("PlotC")
-	)
-)
-,
-fluidRow(
-	column(12,
-	plotOutput("PlotA")
 ))
-))
-
