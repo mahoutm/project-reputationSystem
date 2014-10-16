bb8b0b7ca10bfdf28ec1649b4570b7a16b3c7d09
Hue <hue@sandbox.hortonworks.com>
2014-10-16 14:59:49 +0900

    added to NB framworks based HDFS.

---
+++ build/cat.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class cat{
+        public static void main (String [] args) throws Exception{
+                try{
+                        FileSystem fs = FileSystem.get(new Configuration());
+                        FileStatus[] status = fs.listStatus(new Path("hdfs://jp.seka.com:9000/user/jeka/in"));
+                        for (int i=0;i<status.length;i++){
+                                BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(status[i].getPath())));
+                                String line;
+                                line=br.readLine();
+                                while (line != null){
+                                        System.out.println(line);
+                                        line=br.readLine();
+                                }
+                        }
+                }catch(Exception e){
+                        System.out.println("File not found");
+                }
+        }
+}
