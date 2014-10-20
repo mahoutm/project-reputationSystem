a32ada6cdba6a8cba6a5874f942ff96b339d29f2
repadmin <repadmin@sandbox.hortonworks.com>
2014-10-20 18:29:51 +0900

    updated classfierHD code. v0.2

--- include/ClassifierHD.java
+++ include/ClassifierHD.java
@@ -14,12 +14,13 @@
  * limitations under the License.
  */
 
-/* for PostgreSQL's JDBC
+// for PostgreSQL's JDBC
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-*/
+//import java.sql.ResultSet;
+//import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.math.BigDecimal;
 
 //import java.io.FileReader;
 import java.io.BufferedReader;
@@ -80,9 +81,8 @@ public class ClassifierHD {
 		String labelIndexPath = args[1];
 		String dictionaryPath = args[2];
 		String documentFrequencyPath = args[3];
-		//String tablename = args[4];
+		String tablename = args[4];
 		String inputDir = args[5];
-//		String jobID = args[6];
 		
 	        Configuration configuration = new Configuration();
 
@@ -106,65 +106,106 @@ public class ClassifierHD {
 		System.out.println("Number of labels: " + labelCount);
 		System.out.println("Number of documents in training set: " + documentCount);
 
-                try{
-                        FileSystem fs = FileSystem.get(configuration);
-                        FileStatus[] status = fs.listStatus(new Path(inputDir));
-                        for (int i=0;i<status.length;i++){
-                                BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(status[i].getPath())));
-                                String line;
-                                StringBuilder txt = new StringBuilder();
-                                while ((line = br.readLine()) != null) {
-                                        txt.append(line+'\n');
-                                }
-                                br.close();
+                Connection conn = null;
+                PreparedStatement pstmt = null;
 
-	                	String id = status[i].getPath().getName();
-	                        String message = txt.toString();
-	
-	                        Multiset<String> words = ConcurrentHashMultiset.create();
-	                        
-	                        TokenStream ts = analyzer.tokenStream("text", new StringReader(message));
-	                        CharTermAttribute termAtt = ts.addAttribute(CharTermAttribute.class);
-	                        ts.reset();
-	                        int wordCount = 0;
-	                        while (ts.incrementToken()) {
-	                                if (termAtt.length() > 0) {
-	                                        String word = ts.getAttribute(CharTermAttribute.class).toString();
-	                                        Integer wordId = dictionary.get(word);
-	                                        if (wordId != null) {
-	                                                words.add(word);
-	                                                wordCount++;
-	                                        }
-	                                }
-	                        }
-	                        
-	                        ts.end();
-	                        ts.close();
-	                        
-	                        Vector vector = new RandomAccessSparseVector(10000);
-	                        TFIDF tfidf = new TFIDF();
-	                        for (Multiset.Entry<String> entry:words.entrySet()) {
-	                                String word = entry.getElement();
-	                                int count = entry.getCount();
-	                                Integer wordId = dictionary.get(word);
-	                                Long freq = documentFrequency.get(wordId);
-	                                double tfIdfValue = tfidf.calculate(count, freq.intValue(), wordCount, documentCount);
-	                                vector.setQuick(wordId, tfIdfValue);
-	                        }
-	                        Vector resultVector = classifier.classifyFull(vector);
-	                        double bestScore = -Double.MAX_VALUE;
-	                        int bestCategoryId = -1;
-	                        for(Element element: resultVector.all()) {
-	                                int categoryId = element.index();
-	                                double score = element.get();
-	                                if (score > bestScore) {
-	                                        bestScore = score;
-	                                        bestCategoryId = categoryId;
-	                                }
+                try {
+                Class.forName("org.postgresql.Driver");
+                conn = DriverManager.getConnection("jdbc:postgresql://192.168.50.170:5432/uzeni","postgres", "dbwpsdkdl");
+                conn.setAutoCommit(false);
+                String sql = "INSERT INTO " + tablename + " (id,gtime,wtime,target,num,link,body,rep) VALUES (?,?,?,?,?,?,?,?);";
+                pstmt = conn.prepareStatement(sql);
+                
+                FileSystem fs = FileSystem.get(configuration);
+                FileStatus[] status = fs.listStatus(new Path(inputDir));
+                for (int i=0;i<status.length;i++){
+                        BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(status[i].getPath())));
+			int lv_HEAD = 1;
+			int lv_cnt = 0;
+			String lv_gtime = null;
+			String lv_wtime = null;
+			String lv_target = null;
+			BigDecimal lv_num = null;
+			String lv_link = null;
+                        String[] lv_args;
+                        String lv_line;
+                        StringBuilder lv_txt = new StringBuilder();
+                        while ((lv_line = br.readLine()) != null) {
+				if ( lv_cnt < lv_HEAD ) {
+					lv_args = lv_line.split(",");
+					lv_gtime = lv_args[0];
+					lv_wtime = lv_args[1];
+					lv_target = lv_args[2];
+					lv_num = new BigDecimal(lv_args[3]);
+					lv_link = lv_args[4];
+				} else {
+                                lv_txt.append(lv_line+'\n');
 				}
-				System.out.println(message);
-				System.out.println(" => "+ id + ":" + labels.get(bestCategoryId));
+				lv_cnt++;
+                        }
+                        br.close();
+
+	        	String id = status[i].getPath().getName();
+	                String message = lv_txt.toString();
+	
+	                Multiset<String> words = ConcurrentHashMultiset.create();
+	                
+	                TokenStream ts = analyzer.tokenStream("text", new StringReader(message));
+	                CharTermAttribute termAtt = ts.addAttribute(CharTermAttribute.class);
+	                ts.reset();
+	                int wordCount = 0;
+	                while (ts.incrementToken()) {
+	                        if (termAtt.length() > 0) {
+	                                String word = ts.getAttribute(CharTermAttribute.class).toString();
+	                                Integer wordId = dictionary.get(word);
+	                                if (wordId != null) {
+	                                        words.add(word);
+	                                        wordCount++;
+	                                }
+	                        }
+	                }
+	                
+	                ts.end();
+	                ts.close();
+	                
+	                Vector vector = new RandomAccessSparseVector(10000);
+	                TFIDF tfidf = new TFIDF();
+	                for (Multiset.Entry<String> entry:words.entrySet()) {
+	                        String word = entry.getElement();
+	                        int count = entry.getCount();
+	                        Integer wordId = dictionary.get(word);
+	                        Long freq = documentFrequency.get(wordId);
+	                        double tfIdfValue = tfidf.calculate(count, freq.intValue(), wordCount, documentCount);
+	                        vector.setQuick(wordId, tfIdfValue);
+	                }
+	                Vector resultVector = classifier.classifyFull(vector);
+	                double bestScore = -Double.MAX_VALUE;
+	                int bestCategoryId = -1;
+	                for(Element element: resultVector.all()) {
+	                        int categoryId = element.index();
+	                        double score = element.get();
+	                        if (score > bestScore) {
+	                                bestScore = score;
+	                                bestCategoryId = categoryId;
+	                        }
 			}
+			//System.out.println(message);
+			//System.out.println(" => "+ lv_gtime + lv_wtime + lv_link + id + ":" + labels.get(bestCategoryId));
+                	pstmt.setString(1,id);
+                	pstmt.setString(2,lv_gtime);
+                	pstmt.setString(3,lv_wtime);
+                	pstmt.setString(4,lv_target);
+                	pstmt.setBigDecimal(5,lv_num);
+                	pstmt.setString(6,lv_link);
+                	pstmt.setString(7,message.substring(1,Math.min(50, message.length())));
+                	pstmt.setString(8,labels.get(bestCategoryId));
+		    	pstmt.addBatch();
+		}
+                pstmt.executeBatch();
+    		//pstmt.clearParameters();
+                pstmt.close();
+		conn.commit();
+                conn.close();
                 } catch ( Exception e ) {
                         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
                         System.exit(0);
